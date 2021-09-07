fa70120413af9170e448da514eb728d38098aae5
Igor <70515099+ihornekhaienko@users.noreply.github.com>
2021-09-07 17:49:46 +0300

    Add files via upload

---
+++ RSAEncryption/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace RSAEncryption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        byte[] encrypted;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+                fileTB.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void saveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, fileTB.Text);
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Created by\nihornekhaienko\non 01.24.21");
+        }
+
+        private void encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            RSAEncryptor.GenerateKey();
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            encrypted = RSAEncryptor.Encrypt(fileTB.Text, false);
+            fileTB.Text = ByteConverter.GetString(encrypted);
+        }
+
+        private void decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            fileTB.Text = RSAEncryptor.Decrypt(encrypted, false);
+        }
+    }
+}
---
+++ RSAEncryption/RSAEncryptor.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace RSAEncryption
+{
+    static class RSAEncryptor
+    {
+        static UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        public static string PrivateKey { get; set; }
+        public static string PublicKey { get; set; }
+
+        public static void GenerateKey()
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                PublicKey = RSA.ToXmlString(false);
+                PrivateKey = RSA.ToXmlString(true);
+            }
+
+            using (StreamWriter writer = new StreamWriter("publicKey.xml"))
+            {
+                writer.WriteLine(PublicKey);
+            }
+            using (StreamWriter writer = new StreamWriter("privateKey.xml"))
+            {
+                writer.WriteLine(PrivateKey);
+            }
+        }
+
+        public static string LoadKey()
+        {
+            string key = null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+                key = File.ReadAllText(openFileDialog.FileName);
+
+            return key;
+        }
+
+        public static byte[] Encrypt(string text, bool DoOAEPPadding)
+        {
+            try
+            {
+                string key = LoadKey();
+                if (key == null)
+                {
+                    throw new CryptographicException("key cannot be null");
+                }
+
+                byte[] DataToEncrypt = ByteConverter.GetBytes(text);
+                byte[] encryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.FromXmlString(key);
+
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                MessageBox.Show(e.Message);
+
+                return null;
+            }
+        }
+
+        public static string Decrypt(byte[] DataToDecrypt, bool DoOAEPPadding)
+        {
+            try
+            {
+                string key = LoadKey();
+                if (key == null)
+                {
+                    throw new CryptographicException("key cannot be null");
+                }
+
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.FromXmlString(key);
+
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return ByteConverter.GetString(decryptedData);
+            }
+
+            catch (CryptographicException e)
+            {
+                MessageBox.Show(e.ToString());
+
+                return null;
+            }
+        }
+    }
+}
